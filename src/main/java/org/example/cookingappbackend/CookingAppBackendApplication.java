9137dced4d98630713a591be28bc8160e4359e4d
BartlomiejBartosik <bartos2002@wp.pl>
2025-08-14 12:02:39 +0200

    Inital commit

---
+++ src/main/java/org/example/cookingappbackend/CookingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cookingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cookingappbackend/CookingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cookingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
